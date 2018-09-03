f63fb58d0177364444a6e27a9c18885b0a8d79ff
JackieAlb <xiegz@163.com>
2018-09-03 11:52:45 +0800

    first commit by xiegz

---
+++ src/main/java/cn/xiegz/study/service/Hello.java
@@ -0,0 +1,17 @@
+package cn.xiegz.study.service;
+
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+	
+	@RequestMapping("/hello")
+	public String sayHello() {
+		return "3.hello jenkins "+ new Date();
+	}
+
+}
